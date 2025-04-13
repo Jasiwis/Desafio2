94b0ff91fbc5fa5b3de729a4536948d77da2b35b
Arthur18G <abnernava02@gmail.com>
2025-04-13 16:56:35 -0600

    Feat: Add Desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-04-2025 a las 08:02:26
+-- Tiempo de generación: 14-04-2025 a las 00:55:13
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Versión de PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,6 +36,13 @@ CREATE TABLE `documentos` (
   `fecha_subida` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `documentos`
+--
+
+INSERT INTO `documentos` (`id`, `usuario_id`, `nombre`, `ruta`, `tipo`, `fecha_subida`) VALUES
+(4, 4, 'Blockchain_Trazabilidad_Requisitos_IEEE.docx', 'uploads/user_4/Blockchain_Trazabilidad_Requisitos_IEEE.docx', 'application/vnd.open', '2025-04-13 16:54:24');
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +56,13 @@ CREATE TABLE `usuarios` (
   `password_hash` varchar(255) NOT NULL,
   `fecha_registro` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `email`, `password_hash`, `fecha_registro`) VALUES
+(4, 'Abner Navarro', 'abner@gmail.com', '$2y$10$WLpGIPoePJw9J4FvDUgfnuJMU1XdAkPs7cis84gKqND3s7Nd0uWnu', '2025-04-13 16:54:07');
 
 --
 -- Índices para tablas volcadas
@@ -76,13 +90,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `documentos`
 --
 ALTER TABLE `documentos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
